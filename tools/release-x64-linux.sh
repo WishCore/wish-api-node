d33216b7283b001f11ce78a8a298f3f6e026a88e
Jan Nyman <jan.nyman@controlthings.fi>
2019-05-17 13:30:27 +0300

    Change in Linux x64 build
    Switch to using GCC, and don't use C++11 ABI. This allows binaries
    compiled with GCC5 to work on systems with older libstdc++.

--- tools/release-x64-linux.sh
+++ tools/release-x64-linux.sh
@@ -3,8 +3,8 @@
 ARCH=`node -e "console.log(process.arch+'-'+process.platform)"`
 
 echo "Building for ${ARCH}" 
-cd ..; CC=clang CXX=clang node-gyp rebuild ; 
-#cd ..; CC=gcc CXX=g++ node-gyp rebuild ; 
+#cd ..; CC=clang CXX=clang node-gyp rebuild ; 
+cd ..; CC=gcc CXX=g++ CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 node-gyp rebuild ; 
 cd tools;
 cp ../build/Release/WishApi.node ../bin/WishApi-${ARCH}.node;
 strip ../bin/WishApi-${ARCH}.node;
